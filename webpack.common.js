7e4ed5c0a3822134259456751aade5ed37175726
Luis Pérez <luis.x.perez@macys.com>
2019-04-21 17:04:16 -0300

    Configuração Basica React

--- webpack.common.js
+++ webpack.common.js
@@ -3,45 +3,45 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
- module.exports = {
+module.exports = {
   entry: {
-    app: './src/index.js',
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    app: './src/index.jsx',
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Dito - FE'
-    })
+      title: 'Dito - FE',
+      template: './src/index.html',
+    }),
   ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
-    rules: [{
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/env'], '@babel/react'],
+          },
+        },
+      },
+      {
         test: /\.scss$/,
-        use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader"
-        ]
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
+        use: ['file-loader'],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ]
-      }
-    ]
-  }
-}; 
+        use: ['file-loader'],
+      },
+    ],
+  },
+};
